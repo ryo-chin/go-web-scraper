bf3ef21a14fd1a6f7981379c45dfff170ad83eed
ryo-chin <h031203yama@gmail.com>
2020-07-22 10:10:53 +0900

    feature: basic scraper

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
 func main() {
-	println("start main")
+	url := "https://grips-outdoor.jp/?pid=76851971"
+	resp, err := http.Get(url)
+	if err != nil {
+		println(err)
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(transform.NewReader(resp.Body, japanese.EUCJP.NewDecoder()))
+	if err != nil {
+		panic(err)
+	}
+	if strings.Contains(string(b), "カートに入れる") {
+		fmt.Println(fmt.Sprintf("url= %s は販売中だよ", url))
+	} else {
+		fmt.Println(fmt.Sprintf("url= %s は売り切れ中...", url))
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ryo-chin/go-web-scraper
 
 go 1.12
+
+require golang.org/x/text v0.3.3 // indirect
